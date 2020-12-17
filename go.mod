a36ce6549dd9bb41ab4e557bc78e09948324261f
jones2026 <jones2026@gmail.com>
2020-12-17 01:06:11 -0600

    feat: add metrics endpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/jones2026/go-hello-world
 
 go 1.15
 
-require github.com/go-chi/chi v1.5.1
+require (
+	github.com/go-chi/chi v1.5.1
+	github.com/prometheus/client_golang v1.9.0
+	github.com/slok/go-http-metrics v0.9.0
+)
--- main.go
+++ main.go
@@ -8,12 +8,22 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/jones2026/go-hello-world/internal/handlers"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
+	slokmdlw "github.com/slok/go-http-metrics/middleware"
+	"github.com/slok/go-http-metrics/middleware/std"
 )
 
 func main() {
 	log.Println("Starting app...")
-
 	r := chi.NewRouter()
+
+	prometheusMiddleware := slokmdlw.New(slokmdlw.Config{
+		Recorder: metrics.NewRecorder(metrics.Config{}),
+	})
+
+	r.Use(std.HandlerProvider("", prometheusMiddleware))
+	r.Use(middleware.Heartbeat("/ping"))
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
@@ -22,9 +32,9 @@ func main() {
 
 	r.Get("/", handlers.Hello)
 	r.Get("/healthz", handlers.Health)
+	r.Handle("/metrics", promhttp.Handler())
 
 	port := ":8080"
 	log.Println("Listening on port:", port)
 	log.Fatalln(http.ListenAndServe(port, r))
 }
-
